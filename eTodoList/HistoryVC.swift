30a8fb780c2b4226be68f33943730f0808754940
NightcatWu <nightcatwu@gmail.com>
2016-12-08 23:17:33 -0500

    Init

---
+++ eTodoList/HistoryVC.swift
@@ -0,0 +1,128 @@
+//
+//  HistoryVC.swift
+//  eTodoList
+//
+//  Created by Na Wu on 2016-12-04.
+//  Copyright © 2016 Na Wu. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+
+class HistoryVC: UITableViewController {
+    
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "TodoItemEntity")
+        
+        // Display completed To Do Items
+        fetchRequest.predicate = NSPredicate(format: "isCompleted == true", "isCompleted")
+        
+        do {
+            let results = try managedContext.fetch(fetchRequest)
+            appDelegate.TodoItems = results as! [NSManagedObject]
+            
+        }
+        catch {
+            print ("Fetch error!")
+        }
+        
+        self.tableView.reloadData()
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return appDelegate.TodoItems.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "HistoryCell", for: indexPath)
+
+        // Configure the cell...
+        let item = appDelegate.TodoItems[indexPath.row]
+        cell.textLabel?.text = item.value(forKey: "title") as! String?
+        
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+  
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            //
+            let managedContext = appDelegate.persistentContainer.viewContext
+            managedContext.delete(appDelegate.TodoItems[indexPath.row])
+            appDelegate.saveContext()
+            
+            appDelegate.TodoItems.remove(at: indexPath.row)
+            self.tableView.reloadData()
+
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
